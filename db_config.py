d52fde58c2909c89437773cc1683f6d0bb276f8c
Neeru2001 <141095490+Neeru2001@users.noreply.github.com>
2025-08-14 17:07:35 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,8 +1,1 @@
-# Database configuration details
-db_config = {
-    'host': 'gateway01.us-west-2.prod.aws.tidbcloud.com',
-    'port': 4000,
-    'user': '4Dg3UtBhyBDN7KT.root',      # e.g., 'root'
-    'password': 'e52TkSdKWYtex1Kc',  # The password you set for your MySQL user
-    'database': 'test'    # The database you created in Step 1
-}
+
