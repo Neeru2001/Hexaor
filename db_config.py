d27c16931499aedc1933390b2061cb021b62a067
Neeru2001 <141095490+Neeru2001@users.noreply.github.com>
2025-08-14 13:26:09 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,8 +1,8 @@
 # Database configuration details
 db_config = {
-    'host': 'dpg-d2eoob49c44c7397bsag-a',
-    'port': 5432,
-    'user': 'vehicle_service_user',      # e.g., 'root'
-    'password': 'fATPwI3Y8yTYGwpmzxMlvJ1YXN5G2PnN',  # The password you set for your MySQL user
-    'database': 'vehicle_service'    # The database you created in Step 1
+    'host': 'gateway01.us-west-2.prod.aws.tidbcloud.com',
+    'port': 4000,
+    'user': '4Dg3UtBhyBDN7KT.root',      # e.g., 'root'
+    'password': 'e52TkSdKWYtex1Kc',  # The password you set for your MySQL user
+    'database': 'test'    # The database you created in Step 1
 }
